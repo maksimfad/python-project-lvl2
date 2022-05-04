fe926aed0599a53156abcd319f6bc0243b918eab
maksimfadeev <7522688@gmail.com>
2022-05-04 12:50:38 +0300

    Step 2 (files structure changed, formatt argumen added)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('file', metavar='first_file')
     parser.add_argument('file', metavar='second_file')
+    parser.add_argument('-f','--format', help='set format of output')
     args = parser.parse_args()
     print(args.accumulate(args.integers))
 
