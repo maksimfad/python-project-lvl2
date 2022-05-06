6b0d7a51226ff3472b5eb670a20287a9370f3408
maksimfadeev <7522688@gmail.com>
2022-05-06 13:20:53 +0300

    Step 4 part 1 (linter, codeclimate)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/maksimfad/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/maksimfad/python-project-lvl2/actions)
 [![asciicast](https://asciinema.org/a/JiO0z5kkk4FKOVImR1EYY0mB4.svg)](https://asciinema.org/a/JiO0z5kkk4FKOVImR1EYY0mB4)
+[![Maintainability](https://api.codeclimate.com/v1/badges/4011ef9900bd70aa26a9/maintainability)](https://codeclimate.com/github/maksimfad/python-project-lvl2/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/4011ef9900bd70aa26a9/test_coverage)](https://codeclimate.com/github/maksimfad/python-project-lvl2/test_coverage)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,64 +1,77 @@
 #!/usr/bin/env python
 
-import argparse, json
-from unittest import result
+import argparse
+import json
+# from unittest import result
+
 
 def main():
-    #print("Hello!")
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    # print("Hello!")
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f','--format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    
-    #print(str(args.first_file))
-    #print(args.second_file)
-    
-    def generate_diff(file_path1, file_path2):
-        result = '{\n'
-        #print(file_path1, file_path2)
-        first_file = json.load(open(file_path1))
-        #print(first_file['host'])
-        second_file = json.load(open(file_path2))
-        
-        key_set = set()
-        for key in first_file:
-            key_set.add(key)
-       
-        for key in second_file:
-            key_set.add(key)
-        #print(key_set)
-        key_list = list(key_set)
-        key_list.sort()
-        #print(key_list)
-        for key in key_list:
-            if first_file.get(key) == second_file.get(key):
-                result += '    ' + key + ': ' + converte_output(second_file[key]) + '\n'
-                continue
-            if first_file.get(key) == None:
-                result += '  + ' + key + ': ' + converte_output(second_file[key]) + '\n'
-                continue
-            if second_file.get(key) == None:
-                result += '  - ' + key + ': ' + converte_output(first_file[key]) + '\n'
-                continue
-            if first_file.get(key) != second_file.get(key):
-                result += '  - ' + key + ': ' + converte_output(first_file[key]) + '\n'
-                result += '  + ' + key + ': ' + converte_output(second_file[key]) + '\n'
-        #print(second_file)
-        
-        result += '}'
-        return result
 
-    def converte_output(output):
-        if output == True:
-            output = 'true'
-        if output == False:
-            output = 'false'
-        return str(output)
-        
-    
+    # print(str(args.first_file))
+    # print(args.second_file)
     diff = generate_diff(args.first_file, args.second_file)
     print(diff)
+
+
+def generate_diff(file_path1, file_path2):
+    result = '{\n'
+    # print(file_path1, file_path2)
+    first_file = json.load(open(file_path1))
+    # print(first_file['host'])
+    second_file = json.load(open(file_path2))
+
+    key_list = making_keys_list(first_file, second_file)
+    # print(key_list)
+    for key in key_list:
+        if first_file.get(key) == second_file.get(key):
+            result += ('    ' + key + ': '
+                       + converte_output(second_file[key]) + '\n')
+            continue
+        if first_file.get(key) is None:
+            result += ('  + ' + key + ': '
+                       + converte_output(second_file[key]) + '\n')
+            continue
+        if second_file.get(key) is None:
+            result += ('  - ' + key + ': '
+                       + converte_output(first_file[key]) + '\n')
+            continue
+        if first_file.get(key) != second_file.get(key):
+            result += ('  - ' + key + ': '
+                       + converte_output(first_file[key]) + '\n')
+            result += ('  + ' + key + ': '
+                       + converte_output(second_file[key]) + '\n')
+    # print(second_file)
+
+    result += '}'
+    return result
+
+
+def making_keys_list(first_file, second_file):
+    key_set = set()
+    for key in first_file:
+        key_set.add(key)
+    for key in second_file:
+        key_set.add(key)
+    # print(key_set)
+    key_list = list(key_set)
+    key_list.sort()
+    return key_list
+
+
+def converte_output(output):
+    if output is True:
+        output = 'true'
+    if output is False:
+        output = 'false'
+    return str(output)
+
 
 if __name__ == '__main__':
     main()
